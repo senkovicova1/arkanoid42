c79d1936e041943e55c74d4a6d0e62bbe1161867
senkovicova1 <36665069+senkovicova1@users.noreply.github.com>
2019-01-23 21:51:21 +0100

    Base

---
+++ Assets/Scripts/Ball1.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball1 : MonoBehaviour {
+
+    public float ballInitVelocity = 600f;
+
+    private Rigidbody rb;
+
+    private bool ballInPlay;
+
+    // Use this for initialization
+    void Awake () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.RightControl) && !ballInPlay)
+        {
+            transform.parent = null;
+            ballInPlay = true;
+            rb.isKinematic = false;
+            rb.AddForce(new Vector3(ballInitVelocity, ballInitVelocity, 0));
+        }
+	}
+}
---
+++ Assets/Scripts/Paddle1.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle1 : MonoBehaviour {
+
+    public float paddle1Speed = 1f;
+        
+    private Vector3 paddle1Position = new Vector3(0f, -8f, 0f);
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float xPos = 0;
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            xPos = transform.position.x +  paddle1Speed;
+            paddle1Position = new Vector3(Mathf.Clamp(xPos, -12f, 12f), -8f, 0f);
+            transform.position = paddle1Position;
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            xPos = transform.position.x - paddle1Speed;
+            paddle1Position = new Vector3(Mathf.Clamp(xPos, -12f, 12f), -8f, 0f);
+            transform.position = paddle1Position;
+        }    
+
+    }
+}
